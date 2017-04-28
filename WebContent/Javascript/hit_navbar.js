916a0acd88bf6ef3cf8f1d903a837b07e4fa6c67
arpit2408 <arpit.2303872@gmail.com>
2017-04-28 15:12:29 -0500

    changed faq icon and removed hi and hello alerts

--- WebContent/Javascript/hit_navbar.js
+++ WebContent/Javascript/hit_navbar.js
@@ -1,11 +1,11 @@
-﻿//$(document).ready(function(){
-    function navBarOnLoad() {
-        $("#bs-example-navbar-collapse-1 li a").click(function () {
-            alert("hi");
-            console.log("hello")
-            $('#bs-example-navbar-collapse-1 li a').removeClass('active');
-            $(this).addClass('active')
+﻿////$(document).ready(function(){
+//    function navBarOnLoad() {
+//        $("#bs-example-navbar-collapse-1 li a").click(function () {
+//            alert("hi");
+//            console.log("hello")
+//            $('#bs-example-navbar-collapse-1 li a').removeClass('active');
+//            $(this).addClass('active')
 
 
-        });
-    }
+//        });
+//    }
